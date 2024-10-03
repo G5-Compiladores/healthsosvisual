d23f1e41e44426d0024c93a5ae7eba1fcdddb5a6
Rada749 <radames5897@gmail.com>
2024-10-03 14:55:15 +0200

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,10 +18,10 @@ const Navbar = () => {
       </div>
       <ul className={`fixed shadow-xl top-0 right-0 h-full w-[50%] bg-white flex flex-col items-center justify-center p-4 space-y-4 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out md:relative md:flex md:flex-row md:space-x-6 md:translate-x-0`}>
         <li className="absolute top-4 right-4 md:hidden"><button onClick={toggleMenu} className="text-[#4F93ED]">X</button></li>
-        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row"><i className="fas fa-home block md:hidden my-3"></i><span className="hidden md:block ml-3">Home</span></Link></li>
-        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row"><i className="fas fa-info-circle block md:hidden my-3"></i><span className="hidden md:block ml-3">About</span></Link></li>
-        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row"><i className="fas fa-concierge-bell block md:hidden my-3"></i><span className="hidden md:block ml-3">Services</span></Link></li>
-        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row"><i className="fas fa-envelope block md:hidden my-3"></i><span className="hidden md:block ml-3">Contact</span></Link></li>
+        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row" onClick={toggleMenu}><i className="fas fa-home block md:hidden my-3"></i><span className="hidden md:block ml-3">Home</span></Link></li>
+        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row" onClick={toggleMenu}><i className="fas fa-info-circle block md:hidden my-3"></i><span className="hidden md:block ml-3">About</span></Link></li>
+        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row" onClick={toggleMenu}><i className="fas fa-concierge-bell block md:hidden my-3"></i><span className="hidden md:block ml-3">Services</span></Link></li>
+        <li><Link to="/" className="text-[#4F93ED] flex flex-col items-center md:flex-row" onClick={toggleMenu}><i className="fas fa-envelope block md:hidden my-3"></i><span className="hidden md:block ml-3">Contact</span></Link></li>
       </ul>
     </nav>
   );
